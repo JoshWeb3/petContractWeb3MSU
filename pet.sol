ecacfd63b53588d3a02f1489c57cf7936ed5727c
JoshWeb3 <100609687+JoshWeb3@users.noreply.github.com>
2022-06-27 00:04:51 -0400

    refined pet contract
    added all the hunger functions, and commented out mood
    i propose we show on the frontend the mood of the pet, like if the pet is hungry then it will show it whether the isHungry() function returns false or true, we can expand this system

--- pet.sol
+++ pet.sol
@@ -2,62 +2,75 @@
 
 pragma solidity ^0.8.0;
 
-contract pet {
-    //we can make a constructor if we want, probably saves gas
+import "./petContract.sol";
 
+
+//i suggest we can make it so the frontend will show if the pet is happy
+//or sad depending on if it's hungry or not
+
+
+contract pet {
+    
     constructor(string memory _name) {
         name = _name;
+        lastMeal = block.timestamp;
     }
 
     string name;
     address owner;
-    string mood = "happy";
-    uint age;
+    uint birthday = block.timestamp;
+    uint lastMeal;
+    //string mood;
 
     //setter functions
     function setName(string memory _name) internal {
         name = _name;
     }
 
-    function setMood(string memory _mood) internal {
+/*    function setMood(string memory _mood) internal {
         mood = _mood;
-    }
+ }*/   
 
-    function setAge(uint _age) internal {
-        age = _age;
-    }
 
-    //getter function
+    //getter functions
 
     function getName() public view returns (string memory) {
         return name;
     }
 
-    function getMood() public view returns (string memory) {
-        return mood;
-    }
+/*    function getMood() public view returns (string memory) {
+       return mood;
+   } */
 
+    //returns the age of the pet
     function getAge() internal view returns (uint) {
-        return age;
+        return birthday - block.timestamp;
     }
 
     /* returns the time since last meal */
-    function timeSinceLastMeal() internal returns (uint) {
-        //needs to be built
-        //need to make a way to track the time since last ate
-        //return timeSinceLastMeal;
+    function getTimeSinceLastMeal() internal view returns (uint) {
+        return(lastMeal - block.timestamp);
     }
 
     /*isHungry function that returns whether the pet is hungry or not*/
-    //can go hungry after 1 day 1 week or so
-    //we can make a variable like uint hungerInterval = 1 week
+    //can go hungry after 1 week or whatever specified time
     //more info on time       https://soliditytips.com/articles/solidity-dates-time-operations/
-    function isHungry() internal returns (bool) {
-        //hunger system needs to be built
-        // return hunger;
+    function isHungry() public view returns (bool) {
+        
+        bool hunger;
+        
+        if(block.timestamp >= lastMeal + 1 weeks ){
+            hunger = true;
+        }
+        else{
+        }
+        return hunger;
     }
 
     /*feed function*/
-    //feeding system needs to be built
-    function feed() public {}
+    //feeding system needs to be expanded
+    function feed() public {
+        //need to add more cool stuff, like a charities wallet address
+        lastMeal = block.timestamp;
+    }
 }
